920fa8af1698b1d65c6f87e14829521f61e8ef03
Mavisyang <penny40727@gmail.com>
2018-07-08 20:54:15 +0800

    move file

---
+++ main.js
@@ -0,0 +1,66 @@
+
+var display = '';
+
+// number button
+function btn_num_1() {
+    var num = document.getElementById('btn_num_1');
+    display += num.value;
+    console.log(display);
+}
+
+function btn_num_2() {
+    var num = document.getElementById('btn_num_2');
+    display += num.value;
+    console.log(display);
+    
+}
+
+function btn_num_3() {
+    var num = document.getElementById('btn_num_3');
+    display += num.value;
+    console.log(display);
+}
+
+function btn_num_4() {
+    var num = document.getElementById('btn_num_4');
+    display += num.value;
+    console.log(display);
+}
+
+function btn_num_5() {
+    var num = document.getElementById('btn_num_5');
+    display += num.value;
+    console.log(display);
+}
+
+function btn_num_6() {
+    var num = document.getElementById('btn_num_6');
+    display += num.value;
+    console.log(display);
+}
+
+function btn_num_7() {
+    var num = document.getElementById('btn_num_7');
+    display += num.value;
+    console.log(display);
+}
+
+function btn_num_8() {
+    var num = document.getElementById('btn_num_8');
+    display += num.value;
+    console.log(display);
+}
+
+function btn_num_9() {
+    var num = document.getElementById('btn_num_9');
+    display += num.value;
+    console.log(display);
+}
+
+
+// operator
+function btn_opra_1() {
+    var operator = document.getElementById('btn_opra_1');
+    display += operator.value;
+    console.log(display);
+}
