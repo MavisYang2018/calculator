f4b44979b52184ccdab8af4df330ad153061bc18
Penny.Yang <penny.yang@cvilux.com>
2018-07-09 17:25:18 +0800

    let the string show on the span

--- main.js
+++ main.js
@@ -6,123 +6,125 @@ var display = '';
 function btn_num_1() {
     var num = document.getElementById('btn_num_1');
     display += num.value;
+    document.getElementById('display').innerHTML = display;
+
 }
-
-
 
 
 function btn_num_2() {
     var num = document.getElementById('btn_num_2');
     display += num.value;
-    document.getElementById('display').innerText = 2;
+    document.getElementById('display').innerHTML = display;
     
 }
 
 function btn_num_3() {
     var num = document.getElementById('btn_num_3');
     display += num.value;
-    console.log(display);
+    document.getElementById('display').innerHTML = display;
 }
 
 function btn_num_4() {
     var num = document.getElementById('btn_num_4');
     display += num.value;
-    console.log(display);
+    document.getElementById('display').innerHTML = display;
 }
 
 function btn_num_5() {
     var num = document.getElementById('btn_num_5');
     display += num.value;
-    console.log(display);
+    document.getElementById('display').innerHTML = display;
 }
 
 function btn_num_6() {
     var num = document.getElementById('btn_num_6');
     display += num.value;
-    console.log(display);
+    document.getElementById('display').innerHTML = display;
 }
 
 function btn_num_7() {
     var num = document.getElementById('btn_num_7');
     display += num.value;
-    console.log(display);
+    document.getElementById('display').innerHTML = display;
 }
 
 function btn_num_8() {
     var num = document.getElementById('btn_num_8');
     display += num.value;
-    console.log(display);
+    document.getElementById('display').innerHTML = display;
 }
 
 function btn_num_9() {
     var num = document.getElementById('btn_num_9');
     display += num.value;
-    console.log(display);
+    document.getElementById('display').innerHTML = display;
 }
 
 function btn_num_0() {
     var num = document.getElementById('btn_num_0');
     display += num.value;
-    console.log(display);
+    document.getElementById('display').innerHTML = display;
 }
 
 
 // operator
 function btn_opra_1() {
-    var operator = document.getElementById('btn_opra_1');
-    display += operator.value;
-    console.log(display);
+    var ac = document.getElementById('btn_opra_1');
+    display += ac.value("");
+    document.getElementById('display').innerHTML = display;
 }
 
 function btn_opra_2() {
     var operator = document.getElementById('btn_opra_2');
     display += operator.value;
-    console.log(display);
+    document.getElementById('display').innerHTML = display;
 }
 
 function btn_opra_3() {
     var operator = document.getElementById('btn_opra_3');
     display += operator.value;
-    console.log(display);
+    document.getElementById('display').innerHTML = display;
 }
 
 function btn_opra_4() {
     var operator = document.getElementById('btn_opra_4');
     display += operator.value;
-    console.log(display);
+    document.getElementById('display').innerHTML = display;
 }
 
 function btn_opra_5() {
     var operator = document.getElementById('btn_opra_5');
     display += operator.value;
-    console.log(display);
+    document.getElementById('display').innerHTML = display;
 }
 
 function btn_opra_6() {
     var operator = document.getElementById('btn_opra_6');
     display += operator.value;
-    console.log(display);
+    document.getElementById('display').innerHTML = display;
 }
 
 function btn_opra_7() {
     var operator = document.getElementById('btn_opra_7');
     display += operator.value;
-    console.log(display);
+    document.getElementById('display').innerHTML = display;
 }
 
 function btn_opra_8() {
     var operator = document.getElementById('btn_opra_8');
     display += operator.value;
-    console.log(display);
+    document.getElementById('display').innerHTML = display;
 }
 
 function btn_opra_9() {
     var operator = document.getElementById('btn_opra_8');
     display += operator.value;
-    console.log(display);
+    document.getElementById('display').innerHTML = display;
 }
 
-function show(){
-    var showString = 
-    document.getElementById('display').innerText
-} 
+// function show(){
+//     var showString = 
+//     document.getElementById('display').innerText
+// } 
+
+// ac
